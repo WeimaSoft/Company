a16637683d32860c8d7359dc4cc89fbccdf2b7ad
WeimaSoft <alexandersu@126.com>
2016-10-10 22:31:59 +0800

    Translator

--- Website/Translator/Translator.js
+++ Website/Translator/Translator.js
@@ -1,10 +1,23 @@
 self.setInterval("setupTransform()",5000);
-var imageCount = 1;
+var imageCount = 0;
+var repeatCount = false;
 
 function setupTransform() {
 	imageCount++;
+	
+	if(repeatCount){
+		repeatCount = false;
+	}
+	
 	if(imageCount > 3){
-		imageCount = 0;
+		imageCount = 1;
+		repeatCount = true;
+	}
+	if(repeatCount){
+		document.getElementById("translator").style.transitionDuration = "0ms";
+	}
+	else{
+		document.getElementById("translator").style.transitionDuration = "500ms";
 	}
 	document.getElementById("translator").style.transform = 'translate3d(-' + imageCount * 1000 + 'px,0,0)';
 }
