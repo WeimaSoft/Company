d7b3ca97c34d019457d015a13e062e04d9100bf8
WeimaSoft <alexandersu@126.com>
2016-10-11 22:20:49 +0800

    Translator

--- Website/Translator/Translator.js
+++ Website/Translator/Translator.js
@@ -1,23 +1,27 @@
-self.setInterval("setupTransform()",5000);
-var imageCount = 0;
-var repeatCount = false;
+(function($) {
+	var imageCount = 0;
 
-function setupTransform() {
-	imageCount++;
-	
-	if(repeatCount){
-		repeatCount = false;
-	}
-	
-	if(imageCount > 3){
-		imageCount = 1;
-		repeatCount = true;
-	}
-	if(repeatCount){
-		document.getElementById("translator").style.transitionDuration = "0ms";
-	}
-	else{
-		document.getElementById("translator").style.transitionDuration = "500ms";
-	}
-	document.getElementById("translator").style.transform = 'translate3d(-' + imageCount * 1000 + 'px,0,0)';
-}
+	$.extend({
+		setupTransform : function(translator, imageArrayLength) {
+			imageCount++;
+			if (imageCount == imageArrayLength - 1) {
+				$('#' + translator).css('transition-duration', '500ms');
+				$('#' + translator).css('transform',
+						'translate3d(-' + imageCount * 1000 + 'px,0,0)');
+				imageCount = 0;
+				setTimeout(function() {
+					$('#' + translator).css('transform',
+							'translate3d(-' + imageCount * 1000 + 'px,0,0)');
+					$('#' + translator).css('transition-duration', '0s');
+				}, 500);
+			} else {
+				$('#' + translator).css('transition-duration', '500ms');
+				$('#' + translator).css('transform',
+						'translate3d(-' + imageCount * 1000 + 'px,0,0)');
+			}
+		}
+	})
+
+})(jQuery);
+
+setInterval("$.setupTransform('translator',4)", 5000);
