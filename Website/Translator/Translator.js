b70ee51ec1f0064ab7583332d6d5140fd736e2ca
WeimaSoft <alexandersu@126.com>
2016-10-12 21:09:00 +0800

    finish

--- Website/Translator/Translator.js
+++ Website/Translator/Translator.js
@@ -5,8 +5,9 @@
 
     $.extend(Translator,
     {
-        currentImageIndex: 0,
+        currentImageIndex: 1,
         translatorItemContainer: {},
+        interval:{},
         Options: {
             ImagesCollection: [],
             ImageWidth: 1000,
@@ -16,6 +17,7 @@
             AutoPlayDuration:5000
         },
         Slide: function (translatorContainer, options) {
+        	var self = this;
             $.extend(this.Options, options);
 
             translatorContainer.addClass('translator overflowh');
@@ -23,6 +25,8 @@
             var translatorWrapper = $("<div class='translator-container overflowh'>").appendTo(translatorContainer);
 
             translatorItemContainer = $("<div class='translator-item-container overflowh'></div>").appendTo(translatorWrapper);
+            translatorItemContainer.css('transition-duration', this.Options.TransitionDuration + 'ms');
+            translatorItemContainer.css('transform', 'translate3d(-' + this.Options.ImageWidth + 'px,0,0)');
 
             for (var imgItem in this.Options.ImagesCollection) {
                 var img = this.Options.ImagesCollection[imgItem];
@@ -34,8 +38,12 @@
                 translatorItemContainer.append("<div class='translator-item'><a href='" + img.Link + "'><img src='" + img.Url + "'></a></div>");
             }
             
-            translatorWrapper.append("<div class='translator-btnpreview left0'><div></div></div><div class='translator-btnpreview right0'><div></div></div>");
-
+            var btnPrevious = $("<div class='translator-btnpreview left0'><div></div></div>").appendTo(translatorWrapper);
+            btnPrevious.click(function(){self.ViewPrevious();})
+            
+            var btnNext = $("<div class='translator-btnpreview right0'><div></div></div>").appendTo(translatorWrapper);
+            btnNext.click(function(){self.ViewNext();})
+            
             if (this.Options.DisplayPreview) {
                 translatorWrapper.append("<div class='translator-item-preview' style='right:" + this.Options.ImageWidth + "' ></div><div class='translator-item-preview' style='left:" + this.Options.ImageWidth + "'></div>");
             }
@@ -54,24 +62,48 @@
 
         AutoPlay:function() {
             if (this.Options.AutoPlay) {
-                setInterval("Translator.Play()", this.Options.AutoPlayDuration);
+                this.interval = setInterval("Translator.Play(true)", this.Options.AutoPlayDuration);
             }
         },
-        Play: function() {
-            this.currentImageIndex++;
-            if (this.currentImageIndex == this.Options.ImagesCollection.length) {
+        
+        Play: function(autoPlay) {
+        	if(autoPlay){this.currentImageIndex++;}
+        	var self = this;
+            if (this.currentImageIndex == this.Options.ImagesCollection.length + 1) {
                 translatorItemContainer.css('transition-duration', this.Options.TransitionDuration + 'ms');
                 translatorItemContainer.css('transform', 'translate3d(-' + this.currentImageIndex * this.Options.ImageWidth + 'px,0,0)');
-                this.currentImageIndex = 0;
+                this.currentImageIndex = 1;
                 setTimeout(function () {
-                    translatorItemContainer.css('transform', 'translate3d(0px,0,0)');
+                	translatorItemContainer.css('transform', 'translate3d(-' + self.currentImageIndex * self.Options.ImageWidth + 'px,0,0)');
                     translatorItemContainer.css('transition-duration', '0s');
                 }, 500);
-            } else {
-                translatorItemContainer.css('transition-duration', '500ms');
+            } else if (this.currentImageIndex == 0){
+            	translatorItemContainer.css('transition-duration', this.Options.TransitionDuration + 'ms');
+                translatorItemContainer.css('transform', 'translate3d(-' + this.currentImageIndex * this.Options.ImageWidth + 'px,0,0)');
+                this.currentImageIndex = this.Options.ImagesCollection.length;
+                setTimeout(function () {
+                	translatorItemContainer.css('transform', 'translate3d(-' + self.currentImageIndex * self.Options.ImageWidth + 'px,0,0)');
+                    translatorItemContainer.css('transition-duration', '0s');
+                }, 500);
+            }
+            else {
+                translatorItemContainer.css('transition-duration', this.Options.TransitionDuration + 'ms');
                 translatorItemContainer.css('transform', 'translate3d(-' + this.currentImageIndex * this.Options.ImageWidth + 'px,0,0)');
             }
-        }         
+        },
+        ViewPrevious: function()
+        {
+        	clearInterval(this.interval);
+        	this.currentImageIndex++;
+        	this.Play();
+        	
+        },
+        ViewNext:function(){
+        	clearInterval(this.interval);
+        	this.currentImageIndex--;
+        	this.Play();
+        	
+        }
     });
 })(jQuery);
 
@@ -82,6 +114,7 @@ $(document).ready(function () {
     options.ImagesCollection.push(Translator.CreateImageItem('Images/2.jpg', '#'));
     options.ImagesCollection.push(Translator.CreateImageItem('Images/3.jpg', '#'));
     options.DisplayPreview = false;
+    options.AutoPlay = false;
 
     Translator.Slide($('#example'), options);
 });
