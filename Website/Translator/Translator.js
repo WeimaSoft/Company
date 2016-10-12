4d8ac216a8409eade3cba33a76ae72fb7f2c1eae
WeimaSoft <alexandersu@126.com>
2016-10-12 13:58:46 +0800

    Javascript

--- Website/Translator/Translator.js
+++ Website/Translator/Translator.js
@@ -1,27 +1,87 @@
-(function($) {
-	var imageCount = 0;
+(function ($) {
+    if (typeof Translator === "undefined") {
+        Translator = {};
+    }
 
-	$.extend({
-		setupTransform : function(translator, imageArrayLength) {
-			imageCount++;
-			if (imageCount == imageArrayLength - 1) {
-				$('#' + translator).css('transition-duration', '500ms');
-				$('#' + translator).css('transform',
-						'translate3d(-' + imageCount * 1000 + 'px,0,0)');
-				imageCount = 0;
-				setTimeout(function() {
-					$('#' + translator).css('transform',
-							'translate3d(-' + imageCount * 1000 + 'px,0,0)');
-					$('#' + translator).css('transition-duration', '0s');
-				}, 500);
-			} else {
-				$('#' + translator).css('transition-duration', '500ms');
-				$('#' + translator).css('transform',
-						'translate3d(-' + imageCount * 1000 + 'px,0,0)');
-			}
-		}
-	})
+    $.extend(Translator,
+    {
+        currentImageIndex: 0,
+        translatorItemContainer: {},
+        Options: {
+            ImagesCollection: [],
+            ImageWidth: 1000,
+            TransitionDuration: 500,
+            DisplayPreview: true,
+            AutoPlay: true,
+            AutoPlayDuration:5000
+        },
+        Slide: function (translatorContainer, options) {
+            $.extend(this.Options, options);
 
+            translatorContainer.addClass('translator overflowh');
+
+            var translatorWrapper = $("<div class='translator-container overflowh'>").appendTo(translatorContainer);
+
+            translatorItemContainer = $("<div class='translator-item-container overflowh'></div>").appendTo(translatorWrapper);
+
+            for (var imgItem in this.Options.ImagesCollection) {
+                var img = this.Options.ImagesCollection[imgItem];
+                translatorItemContainer.append("<div class='translator-item'><a href='" + img.Link + "'><img src='" + img.Url + "'></a></div>");
+            }
+
+            for (var i = 0; i < 2; i++) {
+                var img = this.Options.ImagesCollection[i];
+                translatorItemContainer.append("<div class='translator-item'><a href='" + img.Link + "'><img src='" + img.Url + "'></a></div>");
+            }
+            
+            translatorWrapper.append("<div class='translator-btnpreview left0'><div></div></div><div class='translator-btnpreview right0'><div></div></div>");
+
+            if (this.Options.DisplayPreview) {
+                translatorWrapper.append("<div class='translator-item-preview' style='right:" + this.Options.ImageWidth + "' ></div><div class='translator-item-preview' style='left:" + this.Options.ImageWidth + "'></div>");
+            }
+
+            if (this.Options.AutoPlay) {
+                this.AutoPlay();
+            }
+        },
+
+        CreateImageItem: function(url, link) {
+            var img = new Object();
+            img.Url = url;
+            img.Link = link;
+            return img;
+        },
+
+        AutoPlay:function() {
+            if (this.Options.AutoPlay) {
+                setInterval("Translator.Play()", this.Options.AutoPlayDuration);
+            }
+        },
+        Play: function() {
+            this.currentImageIndex++;
+            if (this.currentImageIndex == this.Options.ImagesCollection.length) {
+                translatorItemContainer.css('transition-duration', this.Options.TransitionDuration + 'ms');
+                translatorItemContainer.css('transform', 'translate3d(-' + this.currentImageIndex * this.Options.ImageWidth + 'px,0,0)');
+                this.currentImageIndex = 0;
+                setTimeout(function () {
+                    translatorItemContainer.css('transform', 'translate3d(0px,0,0)');
+                    translatorItemContainer.css('transition-duration', '0s');
+                }, 500);
+            } else {
+                translatorItemContainer.css('transition-duration', '500ms');
+                translatorItemContainer.css('transform', 'translate3d(-' + this.currentImageIndex * this.Options.ImageWidth + 'px,0,0)');
+            }
+        }         
+    });
 })(jQuery);
 
-setInterval("$.setupTransform('translator',4)", 5000);
+$(document).ready(function () {
+    var options = [];
+    options.ImagesCollection = [];
+    options.ImagesCollection.push(Translator.CreateImageItem('Images/1.jpg', '#'));
+    options.ImagesCollection.push(Translator.CreateImageItem('Images/2.jpg', '#'));
+    options.ImagesCollection.push(Translator.CreateImageItem('Images/3.jpg', '#'));
+    options.DisplayPreview = false;
+
+    Translator.Slide($('#example'), options);
+});
